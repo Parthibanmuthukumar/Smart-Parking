5558a8fc05d641a7b39f51a5976a822c9f531c69
Parthibanmuthukumar <160317197+Parthibanmuthukumar@users.noreply.github.com>
2025-03-23 18:16:54 +0530

    Update Login.tsx

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -17,7 +17,6 @@ const Registration = () => {
     navigate('/locations');
     console.log(setStep)
   };
-
   return (
     <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
       <div className="max-w-md w-full bg-white rounded-lg shadow-lg p-8">
